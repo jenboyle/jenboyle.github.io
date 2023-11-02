bce22f88333d1d442fbd93b793db094025efe0f8
jenboyle <groovyjen60@hotmail.com>
2023-11-02 13:51:25 +0000

    search

--- resources/js/transcriptions.js
+++ resources/js/transcriptions.js
@@ -1,5 +1,22 @@
 function goTranscriptionChoice() {
-  console.log('hi');
+  var input, filter, ul, li, a, i, txtValue;
+
+  input = document.getElementById('transcriptionchoice');
+
+  filter = input.value.toUpperCase();
+  ul = document.getElementById('songList');
+  li = ul.getElementsByTagName('li');
+
+  for (i=0; i < li.length; i++){
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+
+    if(txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
 
 }
 
