3299ebfe79c650183fcbeb72b07c479893c6c5a1
jenboyle <groovyjen60@hotmail.com>
2023-11-24 17:49:17 +0000

    random scale picker

--- resources/js/jam.js
+++ resources/js/jam.js
@@ -8,3 +8,24 @@ function playMe(file) {
   document.body.appendChild(audio);
   
 }
+
+function randomScale() {
+
+  fetch('https://groovyjen.com/resources/json/theory/availablescales.json')
+      .then((response) => response.json())
+      .then((json) =>  randomScalePicker(json));
+
+
+}
+
+
+function randomScalePicker(json) {
+
+  const pscale = document.getElementById('randomscale');
+
+  var randSelect = Math.floor(Math.random() * 30) + 1;
+
+
+  pscale.innerHTML = json['scale'+randSelect];
+
+}
