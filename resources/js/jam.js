e624270d87625a90063849bc91bd4de38b5a502b
jenboyle <groovyjen60@hotmail.com>
2024-01-27 15:22:13 +0000

    center the jam buttons

--- resources/js/jam.js
+++ resources/js/jam.js
@@ -11,6 +11,21 @@ var patterns = [{'pattern':'D,no,D,no,D,no,D,no', 'beat': '1,&,2,&,3,&,4,&'},
                   {'pattern':'D,no,D,no,D,no,D,U,D,no,D,no,D,no,D,U', 'beat': '1,e,&,a,2,e,&,a,3,e,&,a,4,e,&,a'},
                   {'pattern':'D,no,D,no,D,no,no,U,D,U,D,no,D,no,D,U', 'beat': '1,e,&,a,2,e,&,a,3,e,&,a,4,e,&,a'}];
 
+var progressions = [{'progression': 'I,V,VI,IV', 'num': '1,5,6,4'},
+                    {'progression': 'I,IV,V', 'num': '1,4,5'},
+                    {'progression': 'I,VI,IV,V', 'num': '1,6,4,5'},
+                    {'progression': 'I,VI,III,VII', 'num': '1,6,3,7'},
+                    {'progression': 'I,VI,II,V', 'num': '1,6,2,5'},
+                    {'progression': 'I,IV', 'num': '1,4'},
+                    {'progression': 'IV,V', 'num': '4,5'},
+                    {'progression': 'I,V,VI,IV', 'num': '1,5,6,4'},
+                    {'progression': 'I,I,I,I,IV,IV,I,I,V,V,I,I', 'num': '1,1,1,1,4,4,1,1,5,5,1,1'},
+                    {'progression': 'II,V,I', 'num': '2,5,1'},
+                    {'progression': 'I,IV,VI,V', 'num': '1,4,6,5'},
+                    {'progression': 'I,III,IV,V', 'num': '1,3,4,5'},
+                    {'progression': 'I,V,VI,III,IV,I,IV,V', 'num': '1,5,6,3,4,1,4,5'},
+                    {'progression': 'I,IV,V,IV', 'num': '1,4,5,4'}];
+
 function playMe(file) {
 
   var audio = document.createElement('audio');
@@ -23,12 +38,9 @@ function playMe(file) {
 }
 
 function randomKey() {
-
   fetch('https://groovyjen.com/resources/json/theory/availablekeys.json')
       .then((response) => response.json())
       .then((json) =>  randomKeyPicker(json));
-
-
 }
 
 
@@ -58,10 +70,10 @@ function randomStrumPattern() {
   guitarpattern.innerHTML = '';
 
   const strum_table = document.createElement("table");
-  const strum_caption = document.createElement("caption");
-  strum_caption.textContent = "Strum Pattern";
-  strum_table.appendChild(strum_caption);
-  strum_table.className = 'strum';
+  //const strum_caption = document.createElement("caption");
+  //strum_caption.textContent = "Strum Pattern";
+  //strum_table.appendChild(strum_caption);
+  strum_table.className = 'randomtables';
   const beat_tr = document.createElement("tr");
 
   const beat = patterns[randompattern]['beat'].split(',');
@@ -115,6 +127,69 @@ function randomStrumPattern() {
                         {'pattern':'D,no,D,U,no,U,D,U', 'beat': '1,&,2,&,3,&,4,&'},
                         {'pattern':'D,no,D,no,D,no,D,U,D,no,D,no,D,no,D,U', 'beat': '1,e,&,a,2,e,&,a,3,e,&,a,4,e,&,a'},
                         {'pattern':'D,no,D,no,D,no,no,U,D,U,D,no,D,no,D,U', 'beat': '1,e,&,a,2,e,&,a,3,e,&,a,4,e,&,a'}];
+    }
+
+
+}
+
+
+function randomChordProgression() {
+  var randomprogression = Math.floor(Math.random() * progressions.length);
+
+  var progression = document.getElementById('randomprogression');
+  progression.innerHTML = '';
+
+  const progression_table = document.createElement("table");
+  //const progression_caption = document.createElement("caption");
+  //progression_caption.textContent = "Chord Progression";
+  //progression_table.appendChild(progression_caption);
+  progression_table.className = 'randomtables';
+  const roman_tr = document.createElement("tr");
+
+  const roman = progressions[randomprogression]['progression'].split(',');
+
+  for(i=0; i<roman.length; i++){
+      const progtd = document.createElement("td");
+      progtd.textContent = roman[i];
+      progtd.className = 'progressioncolour';
+
+      roman_tr.appendChild(progtd);
+    }
+    progression_table.appendChild(roman_tr);
+
+  const num_tr = document.createElement("tr");
+  const num = progressions[randomprogression]['num'].split(',');
+
+  for(i=0; i<num.length; i++){
+    const progtd = document.createElement("td");
+      progtd.textContent = num[i];
+
+
+    num_tr.appendChild(progtd);
+  }
+  progression_table.appendChild(num_tr);
+
+  progression.appendChild(progression_table);
+
+
+    //remove item
+    progressions.splice(randomprogression, 1);
+    if(progressions.length == 0) {
+      //repopulate array
+      progressions = [{'progression': 'I,V,VI,IV', 'num': '1,5,6,4'},
+                                         {'progression': 'I,IV,V', 'num': '1,4,5'},
+                                         {'progression': 'I,VI,IV,V', 'num': '1,6,4,5'},
+                                         {'progression': 'I,VI,III,VII', 'num': '1,6,3,7'},
+                                         {'progression': 'I,VI,II,V', 'num': '1,6,2,5'},
+                                         {'progression': 'I,IV', 'num': '1,4'},
+                                         {'progression': 'IV,V', 'num': '4,5'},
+                                         {'progression': 'I,V,VI,IV', 'num': '1,5,6,4'},
+                                         {'progression': 'I,I,I,I,IV,IV,I,I,V,V,I,I', 'num': '1,1,1,1,4,4,1,1,5,5,1,1'},
+                                         {'progression': 'II,V,I', 'num': '2,5,1'},
+                                         {'progression': 'I,IV,VI,V', 'num': '1,4,6,5'},
+                                         {'progression': 'I,III,IV,V', 'num': '1,3,4,5'},
+                                         {'progression': 'I,V,VI,III,IV,I,IV,V', 'num': '1,5,6,3,4,1,4,5'},
+                                         {'progression': 'I,IV,V,IV', 'num': '1,4,5,4'}];
     }
 
 
