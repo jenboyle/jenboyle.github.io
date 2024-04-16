0b203970efeece49ae2f43f2fbe5a3e6a3087a40
jenboyle <groovyjen60@hotmail.com>
2024-04-16 23:22:33 +0100

    starting typescript

---
+++ resources/ts/test.js
@@ -0,0 +1,3 @@
+var previousRandomIndex = 0;
+var patterns = [];
+patterns.push({ 'pattern': 'D,no,D,no,D,no,D,no', 'beat': '1,&,2,&,3,&,4,&' });
---
+++ resources/ts/test.ts
@@ -0,0 +1,10 @@
+let previousRandomIndex:number = 0;
+
+type Pattern = {
+  pattern: string,
+  beat: string
+}
+
+const patterns: Pattern[] = [];
+patterns.push({'pattern':'D,no,D,no,D,no,D,no', 'beat': '1,&,2,&,3,&,4,&'});
+
