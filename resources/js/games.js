a328c7398c8c90ddb41a7d8bfe2b48928a70e7bc
jenboyle <groovyjen60@hotmail.com>
2024-05-05 11:32:18 +0100

    school chemistry

--- resources/js/games.js
+++ resources/js/games.js
@@ -70,7 +70,7 @@ var circle = [{'key':'C', 'sign': 'no sharps or flats', 'twowrong': '1 sharp,1 f
               {'key':'C#_TOGGLE', 'sign': '7 sharps', 'flatsharp': 'F# C# G# D# A# E# B#', 'twowrong': 'F# C# G# D# A# E# B# C,B# E# A# D# C# G# D#'}];
 
 var chemistryfull = [{'question':'What is meant by the term element?', 'answer': 'CONTAINS(ATOM)ANDCONTAINSOR(ONE_1)', 'help': 'An element is a substance made up of one atom'},
-                 {'question':'What is Chemistry?', 'answer': 'CONTAINS(MATTER)', 'help': 'Chemistry is the study of matter'},
+                 {'question':'What is Chemistry?', 'answer': 'CONTAINS(MATTER)', 'help': 'Chemistry is the study of matter. Matter is anything that occupies space.'},
                  {'question':'What is an atom?', 'answer': 'CONTAINS(PARTICLE)', 'help': 'An atom is a tiny particle'},
                  {'question':'Who created the periodic table of elements?', 'answer': 'CONTAINS(DMITRI)ANDCONTAINS(MENDELEEV)', 'help': 'Dmitri Mendeleev from Russia created the periodic table of elements'},
                  {'question':'Who discovered radium, polenium and treatments for cancer?', 'answer': 'CONTAINS(MARIE)ANDCONTAINS(CURIE)', 'help': 'Marie Curie from Poland and France discovered these'},
@@ -86,26 +86,27 @@ var chemistryfull = [{'question':'What is meant by the term element?', 'answer':
                  {'question':'Name this apparatus to find the temperature of a liquid', 'answer': 'CONTAINS(THERMOMETER)', 'help': 'This is a thermometer', 'img': 'resources/images/games/school/chemistry/thermometer.jpg'},
                  {'question':'You would use this to find the mass of an object', 'answer': 'CONTAINS(TOP PAN BALANCE)', 'help': 'This is a top pan balance', 'img': 'resources/images/games/school/chemistry/toppanbalance.jpg'},
                  {'question':'What is this apparatus?', 'answer': 'CONTAINS(TEST TUBE HOLDER)', 'help': 'This is a test tube holder', 'img': 'resources/images/games/school/chemistry/testtubeholder.jpg'},
-                 {'question':'Name this apparatus?', 'answer': 'CONTAINS(TRIPOD)', 'help': 'This is a tripod', 'img': 'resources/images/games/school/chemistry/tripod.jpg'}];
+                 {'question':'Name this apparatus?', 'answer': 'CONTAINS(TRIPOD)', 'help': 'This is a tripod', 'img': 'resources/images/games/school/chemistry/tripod.jpg'},
+                 {'question':"What is a compound?", 'answer': 'CONTAINS(DIFFERENT)ANDCONTAINS(CHEMICALLY)ONEOF(TWO_2)', 'help': 'A compound is a substance that consists of two or more different elements chemically combined'},
+                 {'question':"Why does Potassium have the symbol K?", 'answer': 'CONTAINS(LATIN)ANDCONTAINS(KALIUM)', 'help': 'Potassium has the symbol K from its Latin name Kalium'},
+                 {'question':"Why does Sodium have the symbol Na?", 'answer': 'CONTAINS(LATIN)ANDCONTAINS(NATRIUM)', 'help': 'Sodium has the symbol Na from its Latin name Natrium'},
+                 {'question':"Why does Copper have the symbol Cu?", 'answer': 'CONTAINS(LATIN)ANDCONTAINS(CUPRIC)', 'help': 'Copper has the symbol Cu from its Latin name Cupric'},
+                 {'question':"Why does Lead have the symbol Pb?", 'answer': 'CONTAINS(LATIN)ANDCONTAINS(PLUM BUM)', 'help': 'Lead(Pb) Latin name Plum Bum'},
+                 {'question':"Why does Iron have the symbol Fe?", 'answer': 'CONTAINS(LATIN)ANDCONTAINS(FERRIS)', 'help': 'Iron(Fe) has the symbol Fe from its Latin name Ferris'},
+                 {'question':"Why can the atomic number also be called?", 'answer': 'CONTAINS(PROTON)', 'help': 'Proton number'},
+                 {'question':"What are vertical columns (up and down) on the periodic table called?", 'answer': 'CONTAINS(GROUP)', 'help': 'Groups', 'img': 'resources/images/games/school/chemistry/group.png'},
+                 {'question':"What are horizontal rows (left to right) on the periodic table called?", 'answer': 'CONTAINS(PERIOD)', 'help': 'Periods', 'img': 'resources/images/games/school/chemistry/period.png'},
+                 {'question':"What is group I on the periodic table called?", 'answer': 'CONTAINS(ALKALI)ANDCONTAINS(METALS)', 'help': 'Group I Alkali metals (very reactive metals)'},
+                 {'question':"What is group VII on the periodic table called for the label in pink on the yellow group?", 'answer': 'CONTAINS(HALOGEN)', 'help': 'Group VII Halogens (reactive non metals)', 'img': 'resources/images/games/school/chemistry/periodic_full_sections_halogens.png'},
+                 {'question':"What is group 0 on the periodic table called for the label in pink on the last group?", 'answer': 'CONTAINS(NOBLE)', 'help': 'Group 0 Noble gases (unreactive gases)', 'img': 'resources/images/games/school/chemistry/periodic_noble.png'},
+                                  ];
 
-var chemistry = [{'question':'What is meant by the term element?', 'answer': 'CONTAINS(ATOM)ANDCONTAINSOR(ONE_1)', 'help': 'An element is a substance made up of one atom'},
-                 {'question':'What is Chemistry?', 'answer': 'CONTAINS(MATTER)', 'help': 'Chemistry is the study of matter'},
-                 {'question':'What is an atom?', 'answer': 'CONTAINS(PARTICLE)', 'help': 'An atom is a tiny particle'},
-                 {'question':'Who created the periodic table of elements?', 'answer': 'CONTAINS(DMITRI)ANDCONTAINS(MENDELEEV)', 'help': 'Dmitri Mendeleev created the periodic table of elements'},
-                 {'question':'Who discovered radium, polenium and treatments for cancer?', 'answer': 'CONTAINS(MARIE)ANDCONTAINS(CURIE)', 'help': 'Marie Curie from Poland and France discovered these'},
-                 {'question':'Who discovered synthetic fibers?', 'answer': 'CONTAINS(STEPHANIE)ANDCONTAINS(KWOLEK)', 'help': 'Stephanie Louise Kwolek discovered synthetic fibers'},
-                 {'question':'In this Oxygen symbol, what does the number 8 represent?', 'answer': 'CONTAINS(ATOMIC)ANDCONTAINS(NUMBER)', 'help': 'This is the atomic number', 'img': 'resources/images/games/school/chemistry/oxygen.png'},
-                 {'question':'In this Oxygen symbol, what does the number 15.999 represent?', 'answer': 'CONTAINS(MASS)', 'help': 'This is the mass number', 'img': 'resources/images/games/school/chemistry/oxygen.png'},
-                 {'question':"Give two features of Mendeleev's periodic table", 'answer': 'TWOOF(INCREASING ATOMIC MASS_NO NOBLE GASES_GAPS_SEPARATE BLOCK)', 'help': '1. Increasing atomic mass, 2. No noble gases not discovered, 3. Gaps for undiscovered, 4. Separate block transition metals'},
-                 {'question':"Give two features of the modern periodic table", 'answer': 'TWOOF(INCREASING ATOMIC NUMBER_NOBLE GASES_NO GAPS_GROUP)', 'help': '1. Increasing atomic number, 2. Noble gases present, 3. No gaps, 4. Separate block transition metals group II and group III'},
-                 {'question':"Is a mixture hard or easy to separate?", 'answer': 'CONTAINS(EASY)', 'help': 'A mixture is easy to separate by filtering and evaporation'},
-                 {'question':'Identify this hazard symbol?', 'answer': 'CONTAINS(EXPLOSIVE)', 'help': 'This is an explosive hazard symbol', 'img': 'resources/images/games/school/chemistry/explosive.jpg'},
-                 {'question':'What is this hazard symbol?', 'answer': 'CONTAINS(FLAMMABLE)', 'help': 'This is a flammable hazard symbol', 'img': 'resources/images/games/school/chemistry/flammable.jpg'},
-                 {'question':'Name this hazard symbol', 'answer': 'CONTAINS(CAUTION)', 'help': 'This is a caution hazard symbol', 'img': 'resources/images/games/school/chemistry/caution.jpg'},
-                 {'question':'Name this apparatus to find the temperature of a liquid', 'answer': 'CONTAINS(THERMOMETER)', 'help': 'This is a thermometer', 'img': 'resources/images/games/school/chemistry/thermometer.jpg'},
-                 {'question':'You would use this to find the mass of an object', 'answer': 'CONTAINS(TOP PAN BALANCE)', 'help': 'This is a top pan balance', 'img': 'resources/images/games/school/chemistry/toppanbalance.jpg'},
-                 {'question':'What is this apparatus?', 'answer': 'CONTAINS(TEST TUBE HOLDER)', 'help': 'This is a test tube holder', 'img': 'resources/images/games/school/chemistry/testtubeholder.jpg'},
-                 {'question':'Name this apparatus?', 'answer': 'CONTAINS(TRIPOD)', 'help': 'This is a tripod', 'img': 'resources/images/games/school/chemistry/tripod.jpg'}];
+//test
+//chemistryfull = [{'question':"What is group 0 on the periodic table called for the label in pink on the last group?", 'answer': 'CONTAINS(NOBLE)', 'help': 'Group 0 Noble gases (unreactive gases)', 'img': 'resources/images/games/school/chemistry/periodic_noble.png'},
+//];
+
+var chemistry = [];
+populateArray(chemistry, chemistryfull);
 
 var biologyfull = [{'question':'What is the nucleus?', 'answer': 'CONTAINS(CONTROL)', 'help': 'The nucleus controls all activities in the cell'},
                    {'question':'What are the 5 senses?', 'answer': 'CONTAINS(SIGHT)ANDCONTAINS(SMELL)ANDCONTAINSTHREE(HEAR)ANDCONTAINSFOUR(TASTE)ANDCONTAINSFIVE(TOUCH)', 'help': 'The 5 senses are Sight, Smell, Hearing, Taste and Touch'},
@@ -141,9 +142,7 @@ var biologyfull = [{'question':'What is the nucleus?', 'answer': 'CONTAINS(CONTR
                    {'question':'What does a plant stem do?', 'answer': 'CONTAINS(HOLDS)ANDCONTAINS(LEAVES)', 'help': 'The stem holds up leaves'},
                    {'question':'What does a plant flower do?', 'answer': 'CONTAINS(REPRODUCTION)', 'help': 'The flowers job is reproduction'},
                    {'question':'What do plant roots do?', 'answer': 'CONTAINS(WATER)', 'help': 'The roots take in water'},
-                   {'question':'What does a plant leaf do?', 'answer': 'CONTAINS(FOOD)', 'help': 'The leaf makes food for the plant'},
-
-                                      ];
+                   {'question':'What does a plant leaf do?', 'answer': 'CONTAINS(FOOD)', 'help': 'The leaf makes food for the plant'}];
 
 var biology = [];
 populateArray(biology, biologyfull);
@@ -486,7 +485,13 @@ function checkAnswer(answerkey, answerselected, nextmethod) {
         }
       }
       console.log(doesItContain);
-      correct = doesItContain;
+      if(oneof == 0) {
+        //only checking oneof
+        correct = doesItContain;
+      } else {
+        //previous clauses
+        correct = correct && doesItContain;
+      }
     }
 
     //TWOOF(INCREASING ATOMIC MASS_NO NOBLE GASES_GAPS_SEPARATE BLOCK)
@@ -648,16 +653,17 @@ function help(){
 //        }
 //    }
         var schoolfulllist = [];
+        var gameSwitch = loadedGame;
 
         if(loadedGame.startsWith('School')) {
           var loadedGameInfo = loadedGame.split('_');
-          loadedGame = loadedGameInfo[0];
+          gameSwitch = loadedGameInfo[0];
           //var schoollist =[{'chemistry': chemistryfull}, {'biology': biologyfull}];
           schoolfulllist = schoollist[loadedGameInfo[1]];
         }
 
 
-        switch(loadedGame) {
+        switch(gameSwitch) {
           case 'Circle':
             if(helpdetail.innerText.length == 0) {
               if(document.getElementById('gamemain').innerHTML.indexOf('flat') != -1){
