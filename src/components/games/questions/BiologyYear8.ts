8cee71ded0866405734a11f45e5056ea46d9e0cb
jenboyle <groovyjen60@hotmail.com>
2024-10-18 21:34:11 +0100

    Fix image issue in games

--- src/components/games/questions/BiologyYear8.ts
+++ src/components/games/questions/BiologyYear8.ts
@@ -189,36 +189,32 @@ export const biologyyear8qs = shuffle([
     help: "The leaf makes food for the plant",
   },
   {
-    question:
-      "In this picture of the brain, what is coloured in green and what is it's purpose?",
+    question: "What brain part is coloured in green and what is it's purpose?",
     answer: "CONTAINS(BRAIN)ANDCONTAINS(STEM)",
     help: "This is the brain stem: In charge of all functions to stay alive eg breathing, circulating blood",
     img: "./images/games/school/biology/brain.jpg",
   },
   {
     question:
-      "In this picture of the brain, what is coloured in yellow? and what is it's purpose?",
+      "What brain part is coloured in yellow? and what is it's purpose?",
     answer: "ONEOF(CEREBRUM_CEREBRAL CORTEX)",
     help: "This is the cerebrum OR cerebral cortex, thinking part of brain, controls voluntary muscles",
     img: "./images/games/school/biology/brain.jpg",
   },
   {
-    question:
-      "In this picture of the brain, what is coloured in blue and what is it's purpose?",
+    question: "What brain part is coloured in blue and what is it's purpose?",
     answer: "CONTAINS(PITUITARY)ANDCONTAINS(HORMONES)",
     help: "This is the pituitary gland it controls hormones",
     img: "./images/games/school/biology/brain.jpg",
   },
   {
-    question:
-      "In this picture of the brain, what is coloured in orange and what is it's purpose?",
+    question: "What brain part is coloured in orange and what is it's purpose?",
     answer: "CONTAINS(THALAMUS)ANDCONTAINS(TEMPERATURE)",
     help: "This is the thalamus or hypothalamus: it controls the temperature of your body",
     img: "./images/games/school/biology/brain.jpg",
   },
   {
-    question:
-      "In this picture of the brain, what is coloured in red  and what is it's purpose?",
+    question: "What brain part is coloured in red  and what is it's purpose?",
     answer: "CONTAINS(CEREBELLUM)ANDCONTAINS(BALANCE)",
     help: "This is the cerebellum: controls balance, movement, coordination, muscles",
     img: "./images/games/school/biology/brain.jpg",
