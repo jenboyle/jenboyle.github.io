44339c17d2220739a1a4190b8ffc55baa5c3efae
jenboyle <jenboyle@users.noreply.github.com>
2024-10-19 16:18:55 +0000

    deploy: 14a04a0ffd55386756a1dd0154955cf42dbd7c74

---
+++ assets/useThemeWithoutDefault-pA1tllC-.js
@@ -0,0 +1,1 @@
+import{r as n}from"./index-BuuF1adu.js";import{T as o}from"./useEnhancedEffect-D2g5buoC.js";function r(t){return Object.keys(t).length===0}function u(t=null){const e=n.useContext(o);return!e||r(e)?t:e}export{u};
