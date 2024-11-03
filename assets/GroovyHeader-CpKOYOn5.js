d1269cd42934cdf25690e0c183179fdcdef82f74
jenboyle <jenboyle@users.noreply.github.com>
2024-11-03 19:33:41 +0000

    deploy: 65c6f1335699334e03925e3d9ef88e361182ac2e

---
+++ assets/GroovyHeader-CpKOYOn5.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-D4u7q7d2.js";import{G as o}from"./GroovyArticle-Ck4L89ly.js";const t="_overridegeneralarticle_1j7ej_1",i={overridegeneralarticle:t};function l({children:e}){return r.jsx(o,{customStyles:i.overridegeneralarticle,children:e})}export{l as G};
