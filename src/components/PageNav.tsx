4e106be981262bc6f1ce2c482cfcd270051e3456
jenboyle <groovyjen60@hotmail.com>
2024-12-05 19:47:25 +0000

    change the nav bar

--- src/components/PageNav.tsx
+++ src/components/PageNav.tsx
@@ -1,6 +1,8 @@
 import { NavLink } from "react-router-dom";
 import styles from "./PageNav.module.css";
-import { IoEllipsisVertical } from "react-icons/io5";
+import { CgClose } from "react-icons/cg";
+import { GiGuitarBassHead } from "react-icons/gi";
+// import { LuListMusic } from "react-icons/lu";
 import { useState } from "react";
 import { useTool } from "../context/ToolContext";
 
@@ -25,55 +27,69 @@ function PageNav() {
     <nav className={styles.nav}>
       <ul onClick={handleNavChange}>
         <li>
-          <NavLink reloadDocument to="/">
+          <NavLink reloadDocument to="/" onClick={hideExcess}>
             Bass Theory
           </NavLink>
         </li>
         <li>
-          <NavLink to="/transcriptions">Transcriptions</NavLink>
+          <NavLink to="/transcriptions" onClick={hideExcess}>
+            Transcriptions
+          </NavLink>
         </li>
 
         <li>
-          <NavLink to="/improvisation">Improvisation</NavLink>
+          <NavLink to="/improvisation" onClick={hideExcess}>
+            Improvisation
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/my_gear">Gear</NavLink>
-        </li>
-
-        <li>
-          <NavLink to="/jam_time">Jam Time</NavLink>
-        </li>
-        <li>
-          <NavLink to="/cubase">Cubase</NavLink>
+          <NavLink to="/my_gear" onClick={hideExcess}>
+            Gear
+          </NavLink>
         </li>
 
         <li>
-          <NavLink reloadDocument to="/games">
+          <NavLink to="/jam_time" onClick={hideExcess}>
+            Jam Time
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/cubase" onClick={hideExcess}>
+            Cubase
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink reloadDocument to="/games" onClick={hideExcess}>
             Games
           </NavLink>
         </li>
         <li>
-          <NavLink reloadDocument to="/blogs">
+          <NavLink reloadDocument to="/blogs" onClick={hideExcess}>
             Blogs
           </NavLink>
         </li>
         <li>
-          <IoEllipsisVertical onClick={handleNavExcess} />
+          {showNavExcess ? (
+            <CgClose onClick={handleNavExcess} />
+          ) : (
+            <GiGuitarBassHead onClick={handleNavExcess} />
+          )}
         </li>
-        {showNavExcess && (
-          <ul onClick={handleNavChange} className={styles.excessnav}>
-            <li onClick={hideExcess}>
-              <NavLink to="/about_me">About Me</NavLink>
-            </li>
-            <li onClick={hideExcess}>
-              <NavLink to="/percussion">Percussion</NavLink>
-            </li>
-            <li onClick={hideExcess}>
-              <NavLink to="/tips">Tips</NavLink>
-            </li>
-          </ul>
-        )}
       </ul>
+      {showNavExcess && (
+        <ul onClick={handleNavChange} className={styles.excessnav}>
+          <li onClick={hideExcess}>
+            <NavLink to="/about_me">About Me</NavLink>
+          </li>
+          <li onClick={hideExcess}>
+            <NavLink to="/percussion">Percussion</NavLink>
+          </li>
+          <li onClick={hideExcess}>
+            <NavLink to="/tips">Tips</NavLink>
+          </li>
+        </ul>
+      )}
     </nav>
   );
 }
