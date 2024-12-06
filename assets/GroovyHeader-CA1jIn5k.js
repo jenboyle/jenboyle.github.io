2491992d94f7eeaba4be36dd9f611676e1d2a526
jenboyle <jenboyle@users.noreply.github.com>
2024-12-06 23:25:47 +0000

    deploy: f4820602b431302d1254fc2bdc73f0412a8e7dbb

---
+++ assets/GroovyHeader-CA1jIn5k.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-DnvDr382.js";import{G as o}from"./GroovyArticle-C8AdpXtI.js";const t="_overridegeneralarticle_1j7ej_1",i={overridegeneralarticle:t};function l({children:e}){return r.jsx(o,{customStyles:i.overridegeneralarticle,children:e})}export{l as G};
