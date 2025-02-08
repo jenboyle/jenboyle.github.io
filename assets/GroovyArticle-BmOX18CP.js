a26ffc4ca30a5d040c4ece1a8b2804b323b5219f
jenboyle <jenboyle@users.noreply.github.com>
2025-02-08 21:31:42 +0000

    deploy: 833c54b25cd044a6466350ce386f55fa94926cf4

---
+++ assets/GroovyArticle-BmOX18CP.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-D5GsaBj-.js";const a="_article_ngufm_1",c={article:a};function s({children:t,customStyles:r}){return e.jsx("article",{className:`${c.article} ${r||""}`,children:t})}export{s as G};
---
+++ assets/GroovyHeader-DLvNAL1O.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-D5GsaBj-.js";import{G as o}from"./GroovyArticle-BmOX18CP.js";const t="_overridegeneralarticle_1j7ej_1",i={overridegeneralarticle:t};function l({children:e}){return r.jsx(o,{customStyles:i.overridegeneralarticle,children:e})}export{l as G};
