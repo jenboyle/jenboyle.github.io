b9dd748b25427d8bdc0ba5bac4328d9111ac1c19
jenboyle <groovyjen60@hotmail.com>
2025-02-12 23:40:57 +0000

    add 2 of my content

--- src/components/songs/mysongs.ts
+++ src/components/songs/mysongs.ts
@@ -1,9 +1,23 @@
 export const songs = [
   {
+    jsonfile: "bait",
+    songtype: "mine",
+    songname: "Bait",
+    key: "Key Bm",
+    link: "bminor",
+  },
+  {
     jsonfile: "communitysucks",
     songtype: "mine",
     songname: "Community Sucks",
     key: "Key Am",
     link: "aminor",
+  },
+  {
+    jsonfile: "trolls",
+    songtype: "mine",
+    songname: "Trolls Have Feelings Too",
+    key: "Key Ab",
+    link: "aflatmajor",
   },
 ];
