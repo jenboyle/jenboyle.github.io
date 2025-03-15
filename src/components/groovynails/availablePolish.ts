73a094735dfcd38069bd29ceab1af9f98e6687e1
jenboyle <groovyjen60@hotmail.com>
2025-03-15 15:39:34 +0000

    Working on the nail designer color picker

---
+++ src/components/groovynails/availablePolish.ts
@@ -0,0 +1,16 @@
+import styles from "../../pages/GroovyNailDesigner.module.css";
+
+export const availablePolish = [
+  {
+    polish: styles.nude,
+  },
+  {
+    polish: styles.red,
+  },
+  {
+    polish: styles.white,
+  },
+  {
+    polish: styles.black,
+  },
+];
--- src/pages/GroovyNailDesigner.tsx
+++ src/pages/GroovyNailDesigner.tsx
@@ -1,12 +1,19 @@
 import { useState } from "react";
 import styles from "./GroovyNailDesigner.module.css";
+import { IoIosArrowDown } from "react-icons/io";
+import { availablePolish } from "../components/groovynails/availablePolish";
 
 function GroovyNailDesigner() {
   const [leftPinkyBaseColor, setLeftPinkyBaseColor] = useState(styles.nude);
   const [rightPinkyBaseColor, setRightPinkyBaseColor] = useState(styles.nude);
+  const [showColorLeftPinky, setShowColorLeftPinky] = useState(false);
 
   function handleLeftPinkyColorChoice(e: React.ChangeEvent<HTMLSelectElement>) {
     setLeftPinkyBaseColor(e.target.value);
+  }
+
+  function handleLeftPinkyColorChoiceM(e: React.MouseEvent<HTMLLIElement>) {
+    setLeftPinkyBaseColor((e.target as HTMLLIElement).id);
   }
 
   function handleRightPinkyColorChoice(
@@ -14,41 +21,45 @@ function GroovyNailDesigner() {
   ) {
     setRightPinkyBaseColor(e.target.value);
   }
+
+  function handleShowColorPickerLeftPinky() {
+    setShowColorLeftPinky(!showColorLeftPinky);
+  }
   return (
     <table>
       <tbody>
-        <tr>
-          <td>
+        <tr className={styles.trs}>
+          <td className={styles.allnails}>
             <div className={`${styles.pinky} ${leftPinkyBaseColor}`}></div>
           </td>
-          <td>
+          <td className={styles.allnails}>
             <div className={styles.ring}></div>
           </td>
-          <td>
+          <td className={styles.allnails}>
             <div className={styles.middle}></div>
           </td>
-          <td>
+          <td className={styles.allnails}>
             <div className={styles.pointer}></div>
           </td>
-          <td>
+          <td className={styles.allnails}>
             <div className={styles.thumb}></div>
           </td>
           <td>
             <div className={styles.spacer}></div>
           </td>
-          <td>
+          <td className={styles.allnails}>
             <div className={styles.thumb}></div>
           </td>
-          <td>
+          <td className={styles.allnails}>
             <div className={styles.pointer}></div>
           </td>
-          <td>
+          <td className={styles.allnails}>
             <div className={styles.middle}></div>
           </td>
-          <td>
+          <td className={styles.allnails}>
             <div className={styles.ring}></div>
           </td>
-          <td>
+          <td className={styles.allnails}>
             <div className={`${styles.pinky} ${rightPinkyBaseColor}`}></div>
           </td>
         </tr>
@@ -85,6 +96,38 @@ function GroovyNailDesigner() {
               <option value={`${styles.white}`}>⚪️</option>
               <option value={`${styles.black}`}>⚫️</option>
             </select>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <div
+              className={styles.colorpicker}
+              onClick={handleShowColorPickerLeftPinky}
+            >
+              <div
+                className={` ${showColorLeftPinky ? "" : leftPinkyBaseColor} ${
+                  showColorLeftPinky ? "" : styles.closeddd
+                } `}
+              >
+                {showColorLeftPinky ? (
+                  <ul>
+                    {availablePolish!.map((polish) => (
+                      <li
+                        key={polish.polish}
+                        id={polish.polish}
+                        value={polish.polish}
+                        className={polish.polish}
+                        onClick={handleLeftPinkyColorChoiceM}
+                      >
+                        test
+                      </li>
+                    ))}
+                  </ul>
+                ) : (
+                  <IoIosArrowDown />
+                )}
+              </div>
+            </div>
           </td>
         </tr>
       </tbody>
