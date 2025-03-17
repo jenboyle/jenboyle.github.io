34c466d94b2047c08966f339b995032d4584ce68
jenboyle <groovyjen60@hotmail.com>
2025-03-17 21:05:36 +0000

    Nail designer add another animal type and white dots

--- src/components/groovynails/availablePatterns.ts
+++ src/components/groovynails/availablePatterns.ts
@@ -9,19 +9,34 @@ export const availablePatterns = [
     pattern: "./images/nails/dots_right.png",
   },
   {
+    pattern: "./images/nails/white_dots_left.png",
+  },
+  {
+    pattern: "./images/nails/white_dots_right.png",
+  },
+  {
     pattern: "./images/nails/blackdots.png",
   },
   {
     pattern: "./images/nails/animal_1.png",
   },
   {
+    pattern: "./images/nails/animal_2.png",
+  },
+  {
+    pattern: "./images/nails/animal_3.png",
+  },
+  {
     pattern: "./images/nails/speckle.png",
+  },
+  {
+    pattern: "./images/nails/white_dots.png",
   },
   {
     pattern: "./images/nails/flower1.png",
   },
   {
-    pattern: "./images/nails/marble_type5.png",
+    pattern: "./images/nails/marble_type7.png",
   },
   {
     pattern: "./images/nails/foil_1.png",
