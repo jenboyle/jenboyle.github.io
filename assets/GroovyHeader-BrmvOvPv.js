f7dc9c9abe5640a7796820d38bdc94c422d4960f
jenboyle <jenboyle@users.noreply.github.com>
2025-03-31 23:27:45 +0000

    deploy: a8088046205a0c3adafc72016ce33f67b1f64f07

---
+++ assets/GroovyHeader-BrmvOvPv.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-Cg-snUwc.js";import{G as o}from"./GroovyArticle-CmW9njgQ.js";const t="_overridegeneralarticle_1j7ej_1",i={overridegeneralarticle:t};function l({children:e}){return r.jsx(o,{customStyles:i.overridegeneralarticle,children:e})}export{l as G};
