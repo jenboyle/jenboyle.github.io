4e361a993f2d6c8f1a5b22845a9a70887a27bef2
jenboyle <jenboyle@users.noreply.github.com>
2025-04-11 19:58:33 +0000

    deploy: 6beab2a2b1b530b54d7e62d4c3f18961ce6ec1d1

---
+++ assets/GroovyHeader-5GpHtzEh.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-m4r0zWzM.js";import{G as o}from"./GroovyArticle-3-jMmKjb.js";const t="_overridegeneralarticle_1j7ej_1",i={overridegeneralarticle:t};function l({children:e}){return r.jsx(o,{customStyles:i.overridegeneralarticle,children:e})}export{l as G};
