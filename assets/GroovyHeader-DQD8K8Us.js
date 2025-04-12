0046f672112ec8725f48de85f405eb4c801407a3
jenboyle <jenboyle@users.noreply.github.com>
2025-04-12 10:44:04 +0000

    deploy: 1dab98017dfde5f18a22252940d5df46ae806e3d

---
+++ assets/GroovyHeader-DQD8K8Us.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-DELeNfv7.js";import{G as o}from"./GroovyArticle-B5yO3TZ7.js";const t="_overridegeneralarticle_1j7ej_1",i={overridegeneralarticle:t};function l({children:e}){return r.jsx(o,{customStyles:i.overridegeneralarticle,children:e})}export{l as G};
