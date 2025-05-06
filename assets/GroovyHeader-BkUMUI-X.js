551afe075d7508d6168f36a23ae67dd44ec79c1a
jenboyle <jenboyle@users.noreply.github.com>
2025-05-06 15:26:02 +0000

    deploy: c14235347b7bd6fa7c224d0547b55ccf8a5fa2e6

---
+++ assets/GroovyHeader-BkUMUI-X.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-8DvvbSOE.js";import{G as o}from"./GroovyArticle-BEScLnjU.js";const t="_overridegeneralarticle_1j7ej_1",i={overridegeneralarticle:t};function l({children:e}){return r.jsx(o,{customStyles:i.overridegeneralarticle,children:e})}export{l as G};
