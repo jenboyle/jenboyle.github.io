0ef449f691605f5f1b841e73b7a8219cc798399a
jenboyle <groovyjen60@hotmail.com>
2025-05-06 12:15:04 +0100

    More HE Qs

--- src/components/games/questions/HEYear9.ts
+++ src/components/games/questions/HEYear9.ts
@@ -1,5 +1,66 @@
 export const heyear9qs = [
   {
+    question: "What is the recommended amount of fibre a day?",
+    answer: "ONEOF(30_THIRTY)",
+    help: "30g per day",
+  },
+  {
+    question: "One reason why fibre is important?",
+    answer: "ONEOF(DIGEST_CONSTIPAT_CANCER_HEART_DIABETES)",
+    help: "Keeps digestive system healthy - food waste travels easily, Prevents constipation and cancer of the bowel, reduces chance of heart disease and type 2 diabetes",
+  },
+  {
+    question: "What is the function of Fibre",
+    answer: "ONEOF(CONSTIPAT_TOXINS_BULK_FULL_SATISFIED)",
+    help: "Prevents constipation, like a sponge mops harmful toxins, bulks food to feel full and satisfied",
+  },
+  {
+    question: "Name a source of Fibre",
+    answer: "ONEOF(WHOLEMEAL_FRUIT_VEG)",
+    help: "Wholemeal bread, wholemeal pasta, fruit and vegetables",
+  },
+  {
+    question: "What is the function of Protein",
+    answer: "ONEOF(GROWTH_REPAIR_TISSUE_MUSCLES_CELLS_ENERGY)",
+    help: "Growth and repair of body tissue, muscles, blood cells. Secondary source of energy",
+  },
+  {
+    question: "Name a source of Protein",
+    answer: "ONEOF(RED MEAT_FISH_EGGS_PULSES_NUTS)",
+    help: "Red meat, fish, eggs, pulses, nuts",
+  },
+  {
+    question: "What is the function of Fat",
+    answer: "ONEOF(ENERGY_ORGANS_INSULAT_CELLS_VITAMIN)",
+    help: "Concentrated source of energy, protect vital organs, insulate body, structure for body cells, provides vitamins ADEK",
+  },
+  {
+    question: "Name a source of Fat",
+    answer: "ONEOF(RED MEAT_BUTTER_MILK_YOGURT_CHEESE)",
+    help: "Red meat, butter, milk, yogurt, cheese",
+  },
+  {
+    question: "What is the function of Carbohydrates",
+    answer: "CONTAINS(ENERGY)",
+    help: "Main source of energy for the body, 50% of our energy should come from this",
+  },
+  {
+    question: "Name a source of Carbohydrates",
+    answer: "ONEOF(BREAD_PASTA_CEREAL_FRUIT_VEGETABLES)",
+    help: "Bread, pasta, cereal, fruit and vegetables",
+  },
+  {
+    question: "What is the function of Iron",
+    answer: "CONTAINS(BLODD)",
+    help: "Healthy blood, forms part of haemoglobin carries oxygen around the body",
+  },
+  {
+    question: "Name a source of Iron",
+    answer:
+      "ONEOF(RED MEAT_GREEN LEAFY VEG_BEANS_FORTIFIED BREAKFAST CEREAL_DRIED FRUIT)",
+    help: "Red meat, green leafy vegetables, beans, fortified breakfast cereal, dried fruit",
+  },
+  {
     question: "What is the function of Calcium",
     answer: "ONEOF(BONE_TEETH)",
     help: "Growth and maintenance strong bones and teeth",
@@ -50,6 +111,104 @@ export const heyear9qs = [
     help: "Sunlight, margarine, eggs, fortified breakfast cereals",
   },
   {
+    question: "What is the function of Water",
+    answer: "ONEOF(TEMP_WASTE_DIGEST_NUTRIENT)",
+    help: "Regulates body temperature, helps excrete waste from body, helps digestion and absorption nutrients",
+  },
+  {
+    question: "What is Yeast",
+    answer:
+      "ONEOF(BIOLOGICAL RAISING AGENT_FUNGUS_RAISING AGENT_MICRO-ORGANISM_MICROSCOPIC FUNGUS)",
+    help: "Biological raising agent | Single celled plant fungus | The raising agent used in bread, doughnuts and buns | A living micro-organism | microscopic fungus consisting of single cells that reproduce by budding and can convert sugar into alcohol and carbon dioxide",
+  },
+  {
+    question: "What is a Micro-organism",
+    answer: "CONTAINS(SMALL)ANDCONTAINS(LIVING)",
+    help: "A living thing too small to be seen with the naked eye eg bacteria",
+  },
+  {
+    question: "What is Fermentation",
+    answer: "CONTAINS(SUGAR)",
+    help: "Metabolic process that converts sugar to acid and gases or alcohol. Occurs in yeast and bacteria",
+  },
+  {
+    question: "What is Budding",
+    answer: "CONTAINS(PARENT)ANDCONTAINS(CELL)",
+    help: "Fully grown parent cell splits into 2 halves, producing 2 new cells",
+  },
+  {
+    question: "What is fungus",
+    answer: "CONTAINS(ORGANISM)",
+    help: "Organism that gets food from decaying material or other living things eg mushroom",
+  },
+  {
+    question: "4 conditions for yeast to grow",
+    answer:
+      "CONTAINS(FOOD)ANDCONTAINS(TIME)ANDCONTAINSTHREE(WARMTH)ANDCONTAINSFOUR(MOISTURE)",
+    help: "Food, time, warmth, moisture",
+  },
+  {
+    question: "Types of vegetarians",
+    answer: "CONTAINS(SEMI)ANDCONTAINS(LACTO)ANDCONTAINSTHREE(VEGAN)",
+    help: "Semi, lacto and vegan. Semi - do not eat red meat will eat chicken and fish. Lacto - do not eat meat, chicken or fish, do eat dairy. Vegan - do not eat meat, chicken, fish or animal products, do not eat dairy or eggs very strict",
+  },
+  {
+    question: "What is a Lacto-ovo vegetarian",
+    answer: "CONTAINS(DAIRY)ANDCONTAINS(EGGS)",
+    help: "Eats dairy and eggs, no meat. Think O-vo O-nly dairy and eggs",
+  },
+  {
+    question: "What is a Lacto-vegetarian",
+    answer: "CONTAINS(DAIRY)",
+    help: "Eats dairy, no meat or eggs",
+  },
+  {
+    question: "What is Vegan",
+    answer: "ONEOF(NO DAIRY_PRODUCT)",
+    help: "No dairy, no eggs, no meat or any by products like honey or gelatine | Person who does not eat or use animal products",
+  },
+  {
+    question: "What is Vegetarian",
+    answer: "CONTAINS(MEAT)",
+    help: "Person does not eat meat or fish and sometimes other animal products for moral, religious or health reasons",
+  },
+  {
+    question: "What is Ethic",
+    answer: "CONTAINS(MORAL)",
+    help: "A set of moral principles",
+  },
+  {
+    question: "What is Lethargy",
+    answer: "ONEOF(ENERGY_ENTHUS)",
+    help: "Lack energy and enthusiasm",
+  },
+  {
+    question: "Why do people become vegetarian?",
+    answer:
+      "ONEOF(HEALTH_ENVIRONMENTAL ISSUES_ANIMAL WELFARE_RELIGION_TASTE_ECONOMIC_PERSONAL)",
+    help: "Health - eg meat high in fat. Environmental issues - eg meat production pollutes. Animal welfare - eg animals being mistreated. Religion - eg Muslims do not eat pork. Taste - do not like the taste. Economic - meat expensive. Personal - eg wrong to kill animals and do not like the taste",
+  },
+  {
+    question: "Two types of iron",
+    answer: "CONTAINS(HAEM)ANDCONTAINS(NON-HAEM)",
+    help: "Haem iron and non-haem iron",
+  },
+  {
+    question: "What is haem iron",
+    answer: "CONTAINS(EAS)",
+    help: "Haem iron is a type of iron in a form EASILY ABSORBED by the body. Red meat, liver and kidney",
+  },
+  {
+    question: "What is non-haem iron and what does it need to help it absorb",
+    answer: "CONTAINS(NOT EAS)ANDCONTAINS(VITAMIN C)",
+    help: "Non-haem iron is a type of iron not EASILY used by the body. It needs vitamin C to help absorb. Fortified breakfast cereals, dried fruit, green leafy vegetables and pulses.",
+  },
+  {
+    question: "What a vegetarian could be missing",
+    answer: "ONEOF(IRON_PROTEIN_B12_CALCIUM_OMEGA)",
+    help: "Iron, protein, B12, Calcium, Omega 3",
+  },
+  {
     question: "What is a consumer?",
     answer: "ONEOF(PURCHASE_BUY)",
     help: "A person who purchases goods or services for personal use",
@@ -88,5 +247,35 @@ export const heyear9qs = [
       "Consumer Rights Act 2015, If the goods are not satisfactory quality, fit for purpose or as described, what can you do?",
     answer: "ONEOF(RETURN_SHOP_RECEIPT_MANAGER_PROBLEM_CAB_CITIZEN_BUREAU)",
     help: "Return to shop, show receipt to manager, explain problem, if it didn't solve you can contact CAB Citizens Advice Bureau",
+  },
+  {
+    question: "What is an allergy",
+    answer: "ONEOF(ABNORMAL_IMMUNE_RESPONSE_REACTION)",
+    help: "Abnormal immune response to a particular food or substance",
+  },
+  {
+    question: "Reasons for increase in allergies",
+    answer: "ONEOF(GENETICS_HYGIENE)",
+    help: "Genetics and hygiene. Genetics - children born into families where allergies exist have a higher chance of allergies. Hygiene - too clean = less exposure to allergens = low immune system = increased risk of allergy",
+  },
+  {
+    question: "How are allergens labelled",
+    answer: "ONEOF(BOLD_HIGHLIGHT_UNDERLINE_ITALIC)",
+    help: "In Bold, highlighted, underlined or italics",
+  },
+  {
+    question: "What is a Food Intolerance",
+    answer: "ONEOF(TROUBLE_DIGEST_IRRITA)",
+    help: "Chemical reaction - when the body has trouble digesting or irritated by certain foods eg sick hours after nuts",
+  },
+  {
+    question: "What is Lactose Intolerance",
+    answer: "ONEOF(LACTOSE_DAIRY)",
+    help: "When the body can't break down or digest lactose (dairy)",
+  },
+  {
+    question: "What is Coeliac Disease",
+    answer: "CONTAINS(GLUTEN)",
+    help: "Small intestine is hypersensitive to gluten",
   },
 ];
