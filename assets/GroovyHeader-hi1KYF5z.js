00afcaf11759c6973dd9083200f377ae24a7e183
jenboyle <jenboyle@users.noreply.github.com>
2025-10-03 23:25:59 +0000

    deploy: bee7d29817ffba43d50915288e0753473a75b1ac

--- assets/GroovyHeader-hi1KYF5z.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-CGOmSKVR.js";import{G as o}from"./GroovyArticle-h7_L1ngS.js";const t="_overridegeneralarticle_1j7ej_1",i={overridegeneralarticle:t};function l({children:e}){return r.jsx(o,{customStyles:i.overridegeneralarticle,children:e})}export{l as G};
