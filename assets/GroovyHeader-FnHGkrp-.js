d765ce5c80c74ff77be0fc1e3f9402767961babc
jenboyle <jenboyle@users.noreply.github.com>
2025-10-13 22:19:15 +0000

    deploy: 9e777e36439c8e91e22589ab90558add74383aed

---
+++ assets/GroovyHeader-FnHGkrp-.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-DmftXciC.js";import{G as o}from"./GroovyArticle-B-mrbM54.js";const t="_overridegeneralarticle_1j7ej_1",i={overridegeneralarticle:t};function l({children:e}){return r.jsx(o,{customStyles:i.overridegeneralarticle,children:e})}export{l as G};
