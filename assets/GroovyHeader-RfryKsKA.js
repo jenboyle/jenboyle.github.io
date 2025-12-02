627ae74f3b773d277d3121533f8a9c884f976e29
jenboyle <jenboyle@users.noreply.github.com>
2025-12-02 23:41:59 +0000

    deploy: d4a7f004bab6a4075de26020e05124ffe6254fa2

---
+++ assets/GroovyHeader-RfryKsKA.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-DDC9aWeD.js";import{G as o}from"./GroovyArticle-DhTcTOTs.js";const t="_overridegeneralarticle_1j7ej_1",i={overridegeneralarticle:t};function l({children:e}){return r.jsx(o,{customStyles:i.overridegeneralarticle,children:e})}export{l as G};
