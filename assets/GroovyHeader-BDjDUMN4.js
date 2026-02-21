dcb195b533616085c3fc7757a707169b136cf894
jenboyle <jenboyle@users.noreply.github.com>
2026-02-21 23:26:12 +0000

    deploy: b4a6d69e4744351659007c03ffe7c6a629f215f4

--- assets/GroovyHeader-BDjDUMN4.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-DQm6cX3-.js";import{G as o}from"./GroovyArticle-Dj2MEK0-.js";const t="_overridegeneralarticle_1j7ej_1",i={overridegeneralarticle:t};function l({children:e}){return r.jsx(o,{customStyles:i.overridegeneralarticle,children:e})}export{l as G};
