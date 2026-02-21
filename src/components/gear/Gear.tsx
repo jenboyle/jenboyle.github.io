b4a6d69e4744351659007c03ffe7c6a629f215f4
jenboyle <groovyjen60@hotmail.com>
2026-02-21 23:24:58 +0000

    Fix some sonarqube and add alt to gear images

--- src/components/gear/Gear.tsx
+++ src/components/gear/Gear.tsx
@@ -5,45 +5,50 @@ import styles from "./Gear.module.css";
 import { allgear } from "./allgear";
 
 export type GearItemArticleType = {
+  id: string;
   article: string;
   image1: string;
   image2?: string;
+  imagealt: string;
   description: string;
   link?: string;
   linktext?: string;
 };
 
 export type GearItemType = {
+  id: string;
   title: string;
   articles: GearItemArticleType[];
 };
 
 function Gear() {
-  return allgear[0].gear.map((gearItem: GearItemType, index) => (
-    <div key={`${gearItem.title}${index}`}>
-      <GroovyHeader key={`gh${index}`}>{gearItem.title}</GroovyHeader>
-      {gearItem.articles.map((articleItem, articleidx) => (
+  return allgear[0].gear.map((gearItem: GearItemType) => (
+    <div key={`${gearItem.title}${gearItem.id}`}>
+      <GroovyHeader key={`gh${gearItem.id}`}>{gearItem.title}</GroovyHeader>
+      {gearItem.articles.map((articleItem) => (
         <GroovyArticle
           customStyles={styles.overridegeneralarticle}
-          key={`ga${articleidx}`}
+          key={`ga${articleItem.id}`}
         >
-          <p key={`p${articleidx}`}>{articleItem.article}</p>
+          <p key={`p${articleItem.id}`}>{articleItem.article}</p>
           {articleItem.image1 ? (
             <img
-              key={`img1${articleidx}`}
+              key={`img1${articleItem.id}`}
               src={articleItem.image1}
               className={styles.boximg}
+              alt={articleItem.imagealt}
             ></img>
           ) : null}
           {articleItem.image2 ? (
             <img
-              key={`img2${articleidx}`}
+              key={`img2${articleItem.id}`}
               src={articleItem.image2}
               className={styles.boximg}
+              alt={articleItem.imagealt}
             ></img>
           ) : null}
 
-          <div key={`desc${articleidx}`} className={styles.div}>
+          <div key={`desc${articleItem.id}`} className={styles.div}>
             {articleItem.description}
           </div>
           {articleItem.link ? (
--- src/components/gear/allgear.ts
+++ src/components/gear/allgear.ts
@@ -2,12 +2,15 @@ export const allgear = [
   {
     gear: [
       {
+        id: "1",
         title: "Gear Wants",
         articles: [
           {
+            id: "1",
             article: "Pedal Wants",
             image1: "./gear/pedals/sy1.jpg",
             image2: "./gear/pedals/plumes.jpg",
+            imagealt: "Pedal Wants",
             description:
               "Thanks to all the pedal pushers this list is ever growing... Starting to think I should maybe just learn how to build my own... 😂 ",
             link: "/pedal_creation",
@@ -16,132 +19,175 @@ export const allgear = [
         ],
       },
       {
+        id: "2",
         title: "Pedals",
         articles: [
           {
+            id: "1",
             article: "My favourite pedals lately",
             image1: "./gear/pedals/discumbobulator.png",
             image2: "./gear/pedals/space.png",
+            imagealt: "Favourite Pedals",
             description: "These two pedals are my favourites lately!",
           },
           {
+            id: "2",
             article: "Drum pedals BeatBuddy and SDRUM",
             image1: "./gear/pedals/beatbuddy.jpg",
             image2: "./gear/pedals/sdrum.jpg",
+            imagealt: "Beat machine",
             description:
               "The beatbuddy is a must for bassists I think to have backing drums for creating solo work. The SDrum is great for when you have a riff and don't know what drum would suit without a drummer.",
           },
           {
+            id: "3",
             article: "Octave Pedals Pog and /boss OC5",
             image1: "./gear/pedals/pog.jpg",
             image2: "./gear/pedals/bossoctave.jpg",
+            imagealt: "Bass Octave",
             description:
               "I had the OC5 first and when I first started using it I think it was helping my ear with the octave. Then I got the POG. I interchange them.",
           },
           {
+            id: "4",
             article: "Dirty Robot",
             image1: "./gear/pedals/dirtyrobot.jpg",
+            imagealt: "Robot",
             description: "I love the noises of this.",
           },
           {
+            id: "5",
             article: "Looper",
             image1: "./gear/pedals/looper.png",
+            imagealt: "Looper",
             description: "I should use this more often.",
           },
           {
+            id: "6",
             article: "Clone",
             image1: "./gear/pedals/clone.png",
+            imagealt: "Bass Clone",
             description: "Doubling bass what's not to like.",
           },
           {
+            id: "7",
             article: "Delay",
             image1: "./gear/pedals/delay.png",
+            imagealt: "Delay",
             description: "Delay pedal",
           },
           {
+            id: "8",
             article: "Distortion",
             image1: "./gear/pedals/muffbig.jpeg",
             image2: "./gear/pedals/muff.jpeg",
+            imagealt: "Distortion",
             description:
               "I should use these more, I was given the silver one by my old band and used it in les pantalons.",
           },
           {
+            id: "9",
             article: "Bad Stone Phase Shifter",
             image1: "./gear/pedals/badstone.jpg",
+            imagealt: "Phase Shifter",
             description:
               "I like the noise of this one. Apparently Thin Lizzy used phasers so I like to think this helps my lizzy covers!",
           },
           {
+            id: "10",
             article: "Bass Envelope Filter",
             image1: "./gear/pedals/bassenv.jpg",
+            imagealt: "Bass Envelope Filter",
             description: "I only got this pedal this year and its a favourite!",
           },
           {
+            id: "11",
             article: "Fuzz",
             image1: "./gear/pedals/fuzz.png",
+            imagealt: "Fuzz",
             description: "This joined the collection about a year ago",
           },
           {
+            id: "12",
             article: "Sidekick",
             image1: "./gear/pedals/sidekick.png",
+            imagealt: "Sidekick",
             description:
               "I used this in my bullet bill song, it sounded pretty good combined with the echo",
           },
           {
+            id: "13",
             article: "Compressors",
             image1: "./gear/pedals/cali.png",
             image2: "./gear/pedals/compressor.jpg",
+            imagealt: "Compressor",
             description:
               "Cali is my new one, the boss one seems to buzz. I find the cali's recommended settings had wild bass tracking, still trying to get the best tone out of it",
           },
           {
+            id: "14",
             article: "JDI",
             image1: "./gear/pedals/jdi.jpeg",
+            imagealt: "DI",
             description:
               "I used to use a stagebug sb2 but I upgraded to this DI.",
           },
           {
+            id: "15",
             article: "Pitch Shifter Brainwaves",
             image1: "./gear/pedals/brainwaves.jpg",
+            imagealt: "Pitch shifter",
             description: "This pedal I have set up for drop half step tuning.",
           },
           {
+            id: "16",
             article: "Polara reverb pedal",
             image1: "./gear/pedals/polara.jpg",
+            imagealt: "Reverb",
             description:
               "I'm not sure this was really a bass pedal but I love the sound and use it a lot.",
           },
           {
+            id: "17",
             article: "Bass Soul Food",
             image1: "./gear/pedals/soul.jpg",
+            imagealt: "Overdrive",
             description:
               "I have this pedal on frequently lately as the overdrive for the envelope filter.",
           },
           {
+            id: "18",
             article: "Tuner",
             image1: "./gear/pedals/tuner.jpg",
+            imagealt: "Tuner",
             description:
               "This tuner is really compact so it leaves space for more pedals and it is always on.",
           },
         ],
       },
       {
+        id: "3",
         title: "Audio Interface",
         articles: [
           {
+            id: "1",
             article: "Focusrite Scarlett 4i4",
             image1: "./gear/interfaces/focusrite.png",
+            imagealt: "Audio interface",
             description:
               "This audio interface is awesome. I originally had a 2i2 and I upgraded to the 4i4 for the midi ports.",
           },
         ],
       },
       {
+        id: "4",
         title: "Keys",
         articles: [
           {
+            id: "1",
             article: "Compact Keys",
             image1: "./gear/keys/keys1.png",
+            imagealt: "Keys",
             image2: "./gear/keys/keys2.png",
             description:
               "My compact keyboards! A Roland JX-08 and AKAI MPK mini play with built in speaker.",
@@ -149,11 +195,14 @@ export const allgear = [
         ],
       },
       {
+        id: "5",
         title: "Sound Acoustics",
         articles: [
           {
+            id: "1",
             article: "Bass Traps",
             image1: "./gear/basstrap.jpg",
+            imagealt: "Bass trap",
             description:
               "I think I noticed a difference when I first got this for the garage. Some people use acoustic foam panels, but I had done some reading and decided to get a bass trap. I have 2 as I ordered one and it went missing so I ordered another and the first showed up a good time later and the company allowed to keep it for free!",
           },
